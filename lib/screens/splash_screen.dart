f66330ee6ac9f25ce1f4901f08d0f28a82236d20
Choi Seunghwan <49161475+Choi-Seunghwan@users.noreply.github.com>
2024-01-17 23:37:40 +0900

    CMT-8 Guest Account 작업 중

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  SplashScreenState createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _checkUserAccount();
+  }
+
+  Future<void> _checkUserAccount() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isUserLoggedIn = prefs.getBool('token') ?? false;
+
+    // token check
+
+    if (isUserLoggedIn) {
+      // 사용자가 로그인한 경우 홈 페이지로 이동
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (_) => HomePage()));
+    } else {
+      // 사용자가 로그인하지 않은 경우 로그인 페이지로 이동
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (_) => LoginPage()));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Loading...'),
+      ),
+    );
+  }
+}
